286b799b45075d695657b1ff74babdc4aa62251f
scarletlvuyyuru <slvuyyuru@hotmail.com>
2024-09-17 14:43:05 -0400

    time date elements added with moment js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-//alert("hello");
+let romeElement = document.querySelector("#rome");
+
+let romeDateElement = romeElement.querySelector(".date");
+let romeTimeElement = romeElement.querySelector(".time");
+let romeTime= moment();
+romeDateElement.innerHTML = moment().format('MMMM Do YYYY');
+romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>]A[</small>]");
