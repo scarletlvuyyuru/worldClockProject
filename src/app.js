8e899f245e9a3d3c7efb1cf3c1c0555bd05484f2
scarletlvuyyuru <slvuyyuru@hotmail.com>
2024-09-18 06:45:27 -0400

    function created for elements

--- src/app.js
+++ src/app.js
@@ -1,35 +1,52 @@
 function updateTime(){
 
 let romeElement = document.querySelector("#rome");
-
+if (romeElement)
+    {
 let romeDateElement = romeElement.querySelector(".date");
 let romeTimeElement = romeElement.querySelector(".time");
 let romeTime= moment.tz("Europe/Rome");
-romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>]A[</small>]");
+romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>] A [</small>]");
 romeDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    }
 
 let honoluluElement = document.querySelector("#honolulu");
-
+if (honoluluElement) 
+    {
 let honoluluDateElement = honoluluElement.querySelector(".date");
 let honoluluTimeElement = honoluluElement.querySelector(".time");
 let honoluluTime= moment.tz("Pacific/Honolulu");
 honoluluTimeElement.innerHTML = honoluluTime.format("h:mm:ss [<small>]A[</small>]");
 honoluluDateElement.innerHTML = moment().format("MMMM Do YYYY");
-
+    }
 let currentElement = document.querySelector("#current");
+if (currentElement) 
+    {
 let currentDateElement = currentElement.querySelector(".date");
 let currentTimeElement = currentElement.querySelector(".time");
 let currentTime= moment();
 
-currentTimeElement.innerHTML = currentTime.format("h:mm:ss SS [<small>]A[</small>]");
+currentTimeElement.innerHTML = currentTime.format("h:mm:ss:SS [<small>] A [</small>]");
 currentDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    }
 }
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
-  console.log(cityTime.format("MMM Do YYY"));
+    
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+
+    <div class="city"> 
+                  <h1>${cityName}</h1>
+                    <div class="date">${cityTime.format("MMM Do YYYY" )}</div>
+                    <div class="time">${cityTime.format("h:mm" )}<small>${cityTime.format("A")}</small></div>
+     </div>`;
+    
 }
+
 
 updateTime();
 setInterval(updateTime, 1000);
