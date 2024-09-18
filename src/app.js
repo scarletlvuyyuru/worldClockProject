7a7e1193a42d84f1281cd41abddc7d8d8fe51371
scarletlvuyyuru <slvuyyuru@hotmail.com>
2024-09-17 22:40:18 -0400

    date and time element added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
+function updateTime(){
+
 let romeElement = document.querySelector("#rome");
 
 let romeDateElement = romeElement.querySelector(".date");
 let romeTimeElement = romeElement.querySelector(".time");
-let romeTime= moment();
-romeDateElement.innerHTML = moment().format('MMMM Do YYYY');
+let romeTime= moment.tz("Europe/Rome");
 romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>]A[</small>]");
+romeDateElement.innerHTML = moment().format("MMMM Do YYYY");
+
+let honoluluElement = document.querySelector("#honolulu");
+
+let honoluluDateElement = honoluluElement.querySelector(".date");
+let honoluluTimeElement = honoluluElement.querySelector(".time");
+let honoluluTime= moment.tz("Pacific/Honolulu");
+honoluluTimeElement.innerHTML = honoluluTime.format("h:mm:ss [<small>]A[</small>]");
+honoluluDateElement.innerHTML = moment().format("MMMM Do YYYY");
+
+let currentElement = document.querySelector("#current");
+let currentDateElement = currentElement.querySelector(".date");
+let currentTimeElement = currentElement.querySelector(".time");
+let currentTime= moment();
+
+currentTimeElement.innerHTML = currentTime.format("h:mm:ss SS [<small>]A[</small>]");
+currentDateElement.innerHTML = moment().format("MMMM Do YYYY");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
